d1d4ab47a7a6f7fec8fcf820109f61833583a4e2
Bruno Pereira <bruno.apereira93@gmail.com>
2019-02-25 14:11:35 +0000

    Roll dice simulator done

---
+++ rolldice.py
@@ -0,0 +1,17 @@
+import random
+
+def roll_dice(n_dices = 1, n_sides = 6):
+    ''''Roll a given number of n-sided dices and return the output as a list
+    Input: n_dices(default = 1)
+           n_sides(default = 6)'''
+    results = []
+    if n_sides <= 0:
+        print('Invalid input')
+        return None
+    else:
+        for x in range(n_dices):
+            results.append(random.randint(0,n_sides))
+    return results
+
+
+print(roll_dice(3, 20))
